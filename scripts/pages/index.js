6807a5ae77ad123eed77f9067aa85bfeeafe0b54
Rudy <Rudy.26.fr@gmail.com>
2023-04-04 09:31:17 +0200

    Init repo

---
+++ scripts/pages/index.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+// Temporaire
+document.querySelectorAll('.dropdown').forEach((b) => {
+    b.addEventListener('click', () => {
+        b.classList.toggle('dropdown--active')
+    })
+})
